6a5767a2529d5f73080236e142afde82a067c022
vjdhama <vjdhama26@gmail.com>
2015-08-10 18:07:29 +0530

    add get input for taking input and delegating call to calculator

--- lib/calculator/calculator_cli.rb
+++ lib/calculator/calculator_cli.rb
@@ -2,12 +2,22 @@ module NumericalCalculator
   #Represents a calculator operation on screen
   class CalculatorCli
 
-    def initialize
-
+    def initialize(calculator)
+      @calculator = calculator
     end
 
     def get_io 
-      input = Kernel.gets
+      input = Kernel.gets.chomp
+    end
+
+    def get_input
+      tokens = get_tokens(get_io)
+      @calculator.add(tokens[1].to_f);
+    end
+
+    private
+    def get_tokens(input)
+      input.split(" ")
     end
   end
 end
--- spec/calculator/calculator_cli_spec.rb
+++ spec/calculator/calculator_cli_spec.rb
@@ -2,11 +2,17 @@ require 'spec_helper'
 
 module NumericalCalculator 
   describe CalculatorCli do 
-    let(:cli) { CalculatorCli.new }
+    let(:calculator) { Calculator.new }
+    let(:cli) { CalculatorCli.new(calculator) }
     
     it 'should get input' do
       allow(Kernel).to receive(:gets).and_return("add 5")
       expect(cli.get_io).to eq("add 5")
+    end
+
+    it "should do foo" do
+      allow(Kernel).to receive(:gets).and_return("add 5")
+      expect(cli.get_input).to eq(5.0)
     end
   end
 end
